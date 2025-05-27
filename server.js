280eb91966770f30241d59bc96238145aa7f6da4
rekinyfilmowe <rekinyfilmowe.krzysiek@gmail.com>
2025-05-27 12:42:25 +0200

    Update server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.post('/send-email', async (req, res) => {
         await transporter.sendMail({
           from: `"Rekiny Filmowe" <${process.env.EMAIL_USER}>`,
           to,
-          bcc: system@rekinyfilmowe.pl,
+          bcc: 'system@rekinyfilmowe.pl',
           subject,
           html,
           attachments
